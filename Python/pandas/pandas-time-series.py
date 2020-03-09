72138a761053958798b695dc991a0639dc5e0221
Andrew Jarombek <ajarombek95@gmail.com>
2020-03-09 19:35:58 -0400

    finish pandas time series work

--- Python/pandas/pandas-time-series.py
+++ Python/pandas/pandas-time-series.py
@@ -111,7 +111,6 @@ assert avg_per_month.index.tz is None
 avg_per_month = avg_per_month.reset_index()
 avg_per_month.columns = ['month', 'average time']
 
-print(avg_per_month['average time'].values)
 assert (avg_per_month['average time'].values == [294, 289.5, 288]).all()
 
 # Reset the index back to the time series
@@ -119,3 +118,95 @@ avg_per_month = avg_per_month.set_index(['month'])
 
 # Localize the time series to NYC time.
 avg_per_month.tz_localize('America/New_York')
+
+# Addition of time intervals respects daylight savings time.
+hour_before_dst = pd.Timestamp('2020-03-08 01:59', tz='US/Eastern')
+assert str(hour_before_dst) == '2020-03-08 01:59:00-05:00'
+
+hour_after_dst = hour_before_dst + Hour()
+assert str(hour_after_dst) == '2020-03-08 03:59:00-04:00'
+
+period: pd.Period = pd.Period('2021', 'A-FEB')
+assert type(period) == pd.Period
+assert period.year == 2021
+
+next_period = period + 1
+assert type(next_period) == pd.Period
+assert next_period.year == 2022
+
+period_start = period.asfreq('D', how='start')
+assert type(period_start) == pd.Period
+assert period_start.year == 2020
+assert period_start.month == 3
+assert period_start.day == 1
+
+period_end = period.asfreq('D', how='end')
+assert type(period_end) == pd.Period
+assert period_end.year == 2021
+assert period_end.month == 2
+assert period_end.day == 28
+
+quarter_range = pd.period_range('2019', '2020', freq='Q-FEB')
+assert quarter_range.dtype == 'period[Q-FEB]'
+
+feb_days = pd.date_range('2020-02-01', periods=29, freq='D')
+run_lengths = np.array([
+    11.56, 12,
+    2.34, 3.63, 2.85, 3.06, 3.92, 7.87, 12.5,
+    2.81, 3.8, 2.65, 7.5, 2.63, 14, 13.21,
+    1.28, 1.88, 2.64, 5.20, 3.76, 7.87, 12.59,
+    2.81, 2.81, 3.45, 2.6, 2.91, 5.2
+])
+feb_runs = pd.Series(run_lengths, feb_days)
+
+# Downsampling to find the average length of a run each week.
+weekly_average_run = feb_runs.resample('W').mean()
+weekly_average_run = weekly_average_run.apply(lambda x: round(x, 2))
+
+assert (weekly_average_run.values == np.array([11.78,  5.17,  6.66,  5.03,  3.3], dtype=object)).all()
+
+weekly_average_run = weekly_average_run.reset_index()
+
+assert (weekly_average_run.values == np.array([
+    [Timestamp('2020-02-02 00:00:00'), 11.78],
+    [Timestamp('2020-02-09 00:00:00'), 5.17],
+    [Timestamp('2020-02-16 00:00:00'), 6.66],
+    [Timestamp('2020-02-23 00:00:00'), 5.03],
+    [Timestamp('2020-03-01 00:00:00'), 3.3]
+], dtype=object)).all()
+
+weekly_average_run = feb_runs.resample('W', label='left').mean()
+weekly_average_run = weekly_average_run.apply(lambda x: round(x, 2)).reset_index()
+
+assert (weekly_average_run.values == np.array([
+    [Timestamp('2020-01-26 00:00:00'), 11.78],
+    [Timestamp('2020-02-02 00:00:00'), 5.17],
+    [Timestamp('2020-02-09 00:00:00'), 6.66],
+    [Timestamp('2020-02-16 00:00:00'), 5.03],
+    [Timestamp('2020-02-23 00:00:00'), 3.3]
+], dtype=object)).all()
+
+weekly_mileage = feb_runs.resample('W', label='left').sum()
+weekly_mileage = weekly_mileage.apply(lambda x: round(x, 2)).reset_index()
+
+assert (weekly_mileage.values == np.array([
+    [Timestamp('2020-01-26 00:00:00'), 23.56],
+    [Timestamp('2020-02-02 00:00:00'), 36.17],
+    [Timestamp('2020-02-09 00:00:00'), 46.60],
+    [Timestamp('2020-02-16 00:00:00'), 35.22],
+    [Timestamp('2020-02-23 00:00:00'), 19.78]
+], dtype=object)).all()
+
+# Upsampling mile and converted 1500m times.
+quarters = [
+    pd.Period('2013Q1'), pd.Period('2014Q1'), pd.Period('2014Q4'), pd.Period('2015Q1'),
+    pd.Period('2016Q1'), pd.Period('2016Q2'), pd.Period('2020Q1')
+]
+times_in_sec = [295, 280, 283, 280, 281, 267, 287]
+mile_progression = pd.Series(times_in_sec, quarters)
+
+# Fill unpopulated quarters with NaN.
+mile_progression.resample('Q').asfreq()
+
+# Forward fill unpopulated quarters.
+mile_progression.resample('Q').ffill()
