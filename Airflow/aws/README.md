30a910c98692b094234309a985264952a2965795
ajarombek <ajarombek95@gmail.com>
2021-11-21 21:33:54 -0500

    more work on airflow tf

---
+++ Airflow/aws/README.md
@@ -0,0 +1,9 @@
+### Overview
+
+Terraform configuration for an Apache Airflow environment on AWS.
+
+### Files
+
+| Filename             | Description                                                                             |
+|----------------------|-----------------------------------------------------------------------------------------|
+| `main.tf`            | Main Terraform script for an Apache Airflow environment using AWS MWAA.                 |
--- Airflow/aws/main.tf
+++ Airflow/aws/main.tf
@@ -25,15 +25,38 @@ terraform {
 
 data "aws_caller_identity" "current" {}
 
+data "aws_vpc" "application-vpc" {
+  tags = {
+    Name = "application-vpc"
+  }
+}
+
+data "aws_subnet" "kubernetes-dotty-public-subnet" {
+  tags = {
+    Name = "kubernetes-dotty-public-subnet"
+  }
+}
+
+data "aws_subnet" "kubernetes-grandmas-blanket-public-subnet" {
+  tags = {
+    Name = "kubernetes-grandmas-blanket-public-subnet"
+  }
+}
+
 resource "aws_mwaa_environment" "sandbox" {
   name = "sandbox"
+  airflow_version = "2.0.2"
+
   source_bucket_arn = aws_s3_bucket.andrew-jarombek-airflow-dags.arn
   dag_s3_path = "dags/"
-  execution_role_arn = ""
+  execution_role_arn = aws_iam_role.airflow.arn
 
   network_configuration {
-    security_group_ids = []
-    subnet_ids = []
+    security_group_ids = [aws_security_group.airflow.id]
+    subnet_ids = [
+      data.aws_subnet.kubernetes-dotty-public-subnet.id,
+      data.aws_subnet.kubernetes-grandmas-blanket-public-subnet.id
+    ]
   }
 
   tags = {
@@ -75,4 +98,65 @@ resource "aws_s3_bucket_policy" "andrew-jarombek-airflow-dags-policy" {
       }
     ]
   })
+}
+
+data "aws_iam_policy_document" "airflow-assume-role-policy" {
+  statement {
+    sid = "mwaa"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      identifiers = ["airflow-env.amazonaws.com", "airflow.amazonaws.com"]
+      type = "service"
+    }
+  }
+}
+
+resource "aws_iam_role" "airflow" {
+  name = "airflow"
+  path = "/sample/"
+  assume_role_policy = data.aws_iam_policy_document.airflow-assume-role-policy.json
+}
+
+data "aws_iam_policy_document" "airflow-policy" {
+  statement {
+
+  }
+}
+
+resource "aws_iam_policy" "airflow" {
+  name = "airflow"
+  path = "/sample/"
+  policy = data.aws_iam_policy_document.airflow-policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "airflow" {
+  policy_arn = aws_iam_policy.airflow.arn
+  role = aws_iam_role.airflow.name
+}
+
+resource "aws_security_group" "airflow" {
+  name = "airflow-sample"
+  description = "Security Group for an Amazon MWAA sample environment"
+  vpc_id = data.aws_vpc.application-vpc.id
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    self = true
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "airflow-sample"
+    Application = "all"
+    Environment = "sandbox"
+  }
 }
