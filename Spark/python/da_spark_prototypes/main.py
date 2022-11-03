9615e392a1f6fdcaa4692b884940dd980bba6951
ajarombek <ajarombek95@gmail.com>
2022-11-02 22:47:27 -0400

    more pyspark code

--- Spark/python/da_spark_prototypes/main.py
+++ Spark/python/da_spark_prototypes/main.py
@@ -8,6 +8,19 @@ import sys
 
 from pyspark.sql import SparkSession, DataFrame
 import pyspark.sql.functions as F
+import pyspark.sql.types as T
+
+
+def spark_session() -> SparkSession:
+    """
+    Get or create a spark session.
+    :return: A spark session object.
+    """
+    return (
+        SparkSession.builder.appName('data-analytics-prototypes')
+        .config("spark.master", "local")
+        .getOrCreate()
+    )
 
 
 def create_df(filename: str) -> DataFrame:
@@ -16,12 +29,7 @@ def create_df(filename: str) -> DataFrame:
     :param filename: Name of a JSON file to read into a dataframe.
     :return: A pyspark dataframe reflecting the data in a JSON file.
     """
-    spark = (
-        SparkSession.builder.appName('data-analytics-prototypes')
-        .config("spark.master", "local")
-        .getOrCreate()
-    )
-
+    spark = spark_session()
     return spark.read.option("multiline", "true").json(filename)
 
 
@@ -48,6 +56,50 @@ def order_by_mileage(data: DataFrame, desc: bool = True) -> DataFrame:
     return data.orderBy(F.desc("miles") if desc else F.col("miles"))
 
 
+def create_exercise_type_table() -> DataFrame:
+    """
+    Create a dataframe containing exercise types.  The dataframe is created using a data set
+    and a programmatically created schema.
+    :return: A dataframe containing exercise types.
+    """
+    spark = spark_session()
+    schema = T.StructType(
+        [
+            T.StructField("id", T.IntegerType(), False),
+            T.StructField("type", T.StringType(), False),
+        ]
+    )
+
+    data = (
+        (1, "run"),
+        (2, "bike"),
+        (3, "kayak"),
+        (4, "core"),
+        (5, "strength"),
+        (6, "downhill ski"),
+        (7, "nordic ski"),
+        (8, "yoga"),
+        (9, "swim"),
+    )
+
+    return spark.createDataFrame(data, schema)
+
+
+def create_languages_table() -> DataFrame:
+    spark = spark_session()
+    schema = "language STRING, first_year_coded INT, total_lines INT, lines ARRAY<INT>"
+
+    data = [
+        ["JavaScript", 2016, 93_692, [42_578, 10_176, 2_499]],
+        ["Python", 2015, 78_580, [16_740, 19_917, 16_415]],
+        ["Java", 2014, 50_122, [2_042, 5_206, 2_724]],
+        ["TypeScript", 2017, 44_290, [11_830, 23_555, 6_036]],
+        ["HTML", 2016, 50_122, [2_484, 4_988, 2_571]],
+    ]
+
+    return spark.createDataFrame(data, schema)
+
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("No JSON File Specified")
@@ -65,3 +117,9 @@ if __name__ == '__main__':
 
     print("Ordered Long Runs:")
     print(order_by_mileage(long_run_df).show())
+
+    print("Exercise Types:")
+    print(create_exercise_type_table().show())
+
+    print("Programming Languages:")
+    print(create_languages_table().show())
--- Spark/python/da_spark_prototypes/test/test_main.py
+++ Spark/python/da_spark_prototypes/test/test_main.py
@@ -11,7 +11,13 @@ import pytest
 from pyspark.sql import DataFrame
 import pyspark.sql.functions as F
 
-from da_spark_prototypes.main import create_df, long_runs, order_by_mileage
+from da_spark_prototypes.main import (
+    create_df,
+    long_runs,
+    order_by_mileage,
+    create_exercise_type_table,
+    create_languages_table,
+)
 
 
 @pytest.fixture()
@@ -22,11 +28,11 @@ def data() -> DataFrame:
     return create_df(filename)
 
 
-def test_create_df(data: DataFrame):
+def test_create_df(data: DataFrame) -> None:
     assert data.count() > 0
 
 
-def test_long_runs(data: DataFrame):
+def test_long_runs(data: DataFrame) -> None:
     long_run_data = long_runs(data)
     assert long_run_data.count() == 2
 
@@ -35,7 +41,7 @@ def test_long_runs(data: DataFrame):
         assert data.get('miles') >= 10
 
 
-def test_order_by_mileage_asc(data: DataFrame):
+def test_order_by_mileage_asc(data: DataFrame) -> None:
     df = data.filter(F.col("type") == "kayak")
     assert df.count() > 0
 
@@ -48,7 +54,7 @@ def test_order_by_mileage_asc(data: DataFrame):
         prev = miles
 
 
-def test_order_by_mileage_desc(data: DataFrame):
+def test_order_by_mileage_desc(data: DataFrame) -> None:
     df = data.filter(F.col("type") == "kayak")
     assert df.count() > 0
 
@@ -59,3 +65,13 @@ def test_order_by_mileage_desc(data: DataFrame):
         miles = row.miles
         assert miles <= prev
         prev = miles
+
+
+def test_create_exercise_type_table() -> None:
+    df = create_exercise_type_table()
+    assert df.count() == 9
+
+
+def test_create_languages_table() -> None:
+    df = create_languages_table()
+    assert df.count() == 5
